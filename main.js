a6c0616b995d50c73fd8ca1b1560ddf68b42f8b6
Florian Liesegang <lysigk@gmail.com>
2022-07-10 16:32:06 +0200

    add amplitude uniform

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 import * as THREE from "https://unpkg.com/three@0.142.0/build/three.module.js";
 
-async function initThree() {
+const ANALYSER_FFT_SIZE = 1024;
+
+async function initAudio() {
+  const audioContext = new AudioContext();
+
+  const node = await new FaustDSP(audioContext, ".").load();
+  node.connect(audioContext.destination);
+
+  const analyser = audioContext.createAnalyser();
+  analyser.fftSize = ANALYSER_FFT_SIZE;
+  node.connect(analyser);
+
+  document.addEventListener("mousemove", (event) => {
+    node.setParamValue("/DSP/mouseX", event.clientX / window.innerWidth);
+    node.setParamValue("/DSP/mouseY", event.clientY / window.innerHeight);
+  });
+
+  return analyser;
+}
+
+async function initThree(analyser) {
   const fragmentShaderResponse = await fetch("shader.frag");
   const fragmentShader = await fragmentShaderResponse.text();
+
+  const clock = new THREE.Clock();
+  const frequenyData = new Uint8Array(analyser.frequencyBinCount);
 
   const scene = new THREE.Scene();
 
@@ -17,10 +40,11 @@ async function initThree() {
     resolution: {
       value: new THREE.Vector2(window.innerWidth, window.innerHeight),
     },
-    time: { value: Date.now() },
     mouse: {
       value: new THREE.Vector2(0.0, 0.0),
     },
+    time: { value: Date.now() },
+    amplitude: { value: 0 },
   };
 
   const geometry = new THREE.PlaneBufferGeometry(2, 2);
@@ -50,10 +74,12 @@ async function initThree() {
     false
   );
 
-  const clock = new THREE.Clock();
-
   function render() {
     uniforms.time.value = clock.getElapsedTime();
+
+    analyser.getByteFrequencyData(frequenyData);
+    const amplitude = frequenyData[20] / 255;
+    uniforms.amplitude.value = amplitude;
 
     renderer.render(scene, camera);
     requestAnimationFrame(render);
@@ -62,21 +88,9 @@ async function initThree() {
   render();
 }
 
-async function initAudio() {
-  const audioContext = new AudioContext();
-
-  const node = await new FaustDSP(audioContext, ".").load();
-  node.connect(audioContext.destination);
-
-  document.addEventListener("mousemove", (event) => {
-    node.setParamValue("/DSP/mouseX", event.clientX / window.innerWidth);
-    node.setParamValue("/DSP/mouseY", event.clientY / window.innerHeight);
-  });
+async function init() {
+  const analyser = await initAudio();
+  initThree(analyser);
 }
 
-function init() {
-  initAudio();
-  initThree();
-}
-
-document.addEventListener("click", init);
+init();
--- shader.frag
+++ shader.frag
@@ -2,14 +2,16 @@ uniform vec2 resolution;
 uniform vec2 mouse;
 
 uniform float time;
+uniform float amplitude;
 
 void main() {
     vec2 uv = gl_FragCoord.xy / resolution.xy;
     uv -= 0.5;
     uv.x *= resolution.x / resolution.y;
 
+    float radius = 0.29 * amplitude + 0.1;
     float blur = 0.1 * abs(sin(time)) + 0.01;
-    float circle = smoothstep(0.3, 0.3 + blur, length(uv));
+    float circle = smoothstep(radius, radius + blur, length(uv));
     vec3 color = vec3(circle, mouse.x, mouse.y);
 
     gl_FragColor = vec4(color, 1.0);
